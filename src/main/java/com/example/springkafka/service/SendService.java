fb684440c0ce22ecc0f520ea93154552967fe114
Alexey Leping <aleping@yandex.ru>
2022-11-06 18:15:00 +0300

    add generator

--- src/main/java/com/example/springkafka/service/SendService.java
+++ src/main/java/com/example/springkafka/service/SendService.java
@@ -10,24 +10,20 @@ import java.util.List;
 
 @Service
 public class SendService {
-
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    static private String topic;
-    List<AppUserDto> appUserDtoList = new ArrayList<>();
     public SendService(KafkaTemplate<String, Object> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void generationDto(RecordData recordData){
+        String topic;
+        List<AppUserDto> appUserDtoList = new ArrayList<>();
         int count = recordData.size();
         topic = recordData.topic();
         for(int i = 0; i<count; i++){
             appUserDtoList.add(new AppUserDto());
         }
-    }
-
-    public void produce() {
         kafkaTemplate.send(topic, appUserDtoList);
     }
 }
