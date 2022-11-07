62f3b6120646e2fa29886bca911cabbdc31fa164
Alexey Leping <alexeyleping@yandex.ru>
2022-11-07 10:13:55 +0300

    add open api

--- src/main/java/com/example/springkafka/dto/AppUserDto.java
+++ src/main/java/com/example/springkafka/dto/AppUserDto.java
@@ -1,15 +1,16 @@
 package com.example.springkafka.dto;
 
+import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
 public class AppUserDto {
 
     private String name;
-    private Date date;
+    private LocalDateTime date;
 
     public AppUserDto() {
         this.name = "IRobot";
-        this.date = Calendar.getInstance().getTime();
+        this.date = LocalDateTime.now();
     }
 }
